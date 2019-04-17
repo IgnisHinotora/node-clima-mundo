be1e134fc10b8a68ca3e097a7676a0fb2c63f558
IgnisHinotora <Alvaro Vidal@DesarrolloMB.local>
2019-04-17 17:43:38 -0400

    Últimos cambios

--- lugar/lugar.js
+++ lugar/lugar.js
@@ -3,18 +3,18 @@ const axios = require('axios');
 
 const getLugarLatLng = async(dir) => { // se agrega Async a la primera version de ésta función
 
-    const encodedUrl = encodeURI(dir);
+    const encodedUrl = encodeURI(dir); //Codifica el parámetro para leer espacios entre palabras
 
     console.log(encodedUrl);
 
-    var instance = axios.create({
+    var instance = axios.create({ //Crea una instancia del servicio
         baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedUrl}`,
         headers: { 'X-RapidAPI-Key': 'd998d2fe2emshca1129fb47ef28dp1e778ajsn20e6f6866009' }
     });
 
-    const resp = await instance.get();
+    const resp = await instance.get(); //Respuesta obtiene la respuesta del get al sevicio instanciado
 
-    if (resp.data.Results.length === 0) {
+    if (resp.data.Results.length === 0) { //La respuesta debe tener un largo desde 1 en adelante
         throw new Error(`No hay resultados para ${direccion}`);
     }
 
